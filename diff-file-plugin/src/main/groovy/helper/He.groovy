f08f9d5d257b785a3b79d2c50cac174d47292962
zhibin ren <zhibin.ren@thoughtworks.com>
2020-02-27 21:34:45 +0800

    test diff file

--- diff-file-plugin/src/main/groovy/helper/He.groovy
+++ diff-file-plugin/src/main/groovy/helper/He.groovy
@@ -9,34 +9,31 @@ import org.gradle.internal.impldep.org.eclipse.jgit.treewalk.CanonicalTreeParser
 public class He {
 
     public static void main(String[] args) throws IOException, GitAPIException {
-        try (Repository repository = JGitHelper.openJGitCookbookRepository()) {
-            // The {tree} will return the underlying tree-id instead of the commit-id itself!
-            // For a description of what the carets do see e.g. http://www.paulboxley.com/blog/2011/06/git-caret-and-tilde
-            // This means we are selecting the parent of the parent of the parent of the parent of current HEAD and
-            // take the tree-ish of it
-            ObjectId oldHead = repository.resolve("HEAD^{tree}");
-            ObjectId head = repository.resolve("HEAD{tree}");
+        Repository repository = JGitHelper.openJGitCookbookRepository()
+        // The {tree} will return the underlying tree-id instead of the commit-id itself!
+        // For a description of what the carets do see e.g. http://www.paulboxley.com/blog/2011/06/git-caret-and-tilde
+        // This means we are selecting the parent of the parent of the parent of the parent of current HEAD and
+        // take the tree-ish of it
+        ObjectId oldHead = repository.resolve("HEAD^{tree}");
+        ObjectId head = repository.resolve("HEAD{tree}");
 
-            System.out.println("Printing diff between tree: " + oldHead + " and " + head);
+        System.out.println("Printing diff between tree: " + oldHead + " and " + head);
 
-            // prepare the two iterators to compute the diff between
-            try (ObjectReader reader = repository.newObjectReader()) {
-                CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
-                oldTreeIter.reset(reader, oldHead);
-                CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
-                newTreeIter.reset(reader, head);
+        // prepare the two iterators to compute the diff between
+        ObjectReader reader = repository.newObjectReader()
+        CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
+        oldTreeIter.reset(reader, oldHead);
+        CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
+        newTreeIter.reset(reader, head);
 
-                // finally get the list of changed files
-                try (Git git = new Git(repository)) {
-                    List<DiffEntry> diffs = git.diff()
-                        .setNewTree(newTreeIter)
-                        .setOldTree(oldTreeIter)
-                        .call();
-                    for (DiffEntry entry : diffs) {
-                        System.out.println("Entry: " + entry);
-                    }
-                }
-            }
+        // finally get the list of changed files
+        Git git = new Git(repository)
+        List<DiffEntry> diffs = git.diff()
+            .setNewTree(newTreeIter)
+            .setOldTree(oldTreeIter)
+            .call();
+        for (DiffEntry entry : diffs) {
+            System.out.println("Entry: " + entry);
         }
 
         System.out.println("Done");
